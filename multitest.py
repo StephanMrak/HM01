b73208640d138d3091292f6c09759283ba0cc8ee
Alex <16436376+gr33nhub@users.noreply.github.com>
2024-10-13 12:20:13 +0200

    Experiments with multiprocessing

---
+++ multitest.py
@@ -0,0 +1,50 @@
+import multiprocessing.process
+import multiprocessing
+import multitest_functions
+
+
+if __name__ == "__main__":
+
+    with multiprocessing.Manager() as manager:
+        # is false at the beginning. Processes that call wait() will block until set() is called to set the internal flag to true.
+        event_warmup = manager.Event()
+        event_stop = manager.Event()
+        queue_hit = manager.Queue(maxsize=1)
+        namespace = manager.Namespace()
+        namespace.size = (1360, 768)
+        namespace.ii = 0
+
+        t0 = multiprocessing.Process(
+            name="Warmup",
+            target=multitest_functions.Warmup,
+            args=(event_warmup,),
+        )
+        t0.start()
+
+        t1 = multiprocessing.Process(
+            name="Server",
+            target=multitest_functions.myServer,
+            args=(
+                event_warmup,
+                event_stop,
+                queue_hit,
+                namespace,
+            ),
+        )
+        t1.start()
+
+        t2 = multiprocessing.Process(
+            name="Counter",
+            target=multitest_functions.Counter,
+            args=(
+                event_warmup,
+                event_stop,
+                queue_hit,
+                namespace,
+            ),
+        )
+        t2.start()
+
+        t0.join()
+        t1.join()
+        t2.join()
---
+++ multitest_functions.py
@@ -0,0 +1,119 @@
+import remi.gui as gui
+from remi import start, App, Server
+import time
+import multiprocessing
+import queue
+from random import randint
+from datetime import datetime
+
+
+class MyApp(App):
+    def __init__(self, *args):
+        # self.E1 = E1
+        # self.Q1 = Q1
+        # print(self.E1)
+        # print(self.Q1)
+        # self.E1 = args.pop("E1")
+        # self.Q1 = args.pop("Q1")
+        super(MyApp, self).__init__(*args)
+
+    def main(self, e_warmup, e_stop, q_hit, shared_ns):
+        self.e_warmup = e_warmup
+        self.e_stop = e_stop
+        self.q_hit = q_hit
+        self.shared_ns = shared_ns
+
+        self.size = (1360, 768)
+        container = gui.VBox(width=120, height=100)
+        self.lbl = gui.Label("Hello world!")
+
+        self.bt = gui.Button("Press me!")
+        self.bt.onclick.do(self.on_button_pressed)
+
+        self.bt_stop = gui.Button("Stop")
+        self.bt_stop.onclick.do(self.button_stop_pressed)
+
+        # appending a widget to another, the first argument is a string key
+        container.append(self.lbl)
+        container.append(self.bt)
+        container.append(self.bt_stop)
+
+        # returning the root widget
+        return container
+
+    # listener function
+    def on_button_pressed(self, widget):
+        self.bt.set_text("Press me again!")
+        # print(self.shared_ns)
+        # print(self.shared_ns.ii)
+        # print(self.e_warmup)
+        # print(self.e_warmup.is_set())
+        print("MyApp: read shared_ns.ii", self.shared_ns.ii)
+
+        x_pixel = randint(0, self.size[0] - 1)
+        y_pixel = randint(0, self.size[1] - 1)
+        hit = {
+            "x_pixel": x_pixel,
+            "y_pixel": y_pixel,
+            "x_percent": x_pixel / self.size[0],
+            "y_percent": y_pixel / self.size[1],
+            "datetime": datetime.now(),
+        }
+        try:
+            self.q_hit.put_nowait(hit)
+        except queue.Full:
+            raise Exception("Hit Queue is full.")
+        print("MyApp: put a random hit in the queue q_hit", hit)
+
+    def button_stop_pressed(self, widget):
+        # self.server.server_starter_instance._alive = False
+        # self.server.server_starter_instance._sserver.shutdown()
+        self.server.server_starter_instance.stop()
+        print("server stopped")
+        self.e_stop.set()
+
+
+def myServer(e_warmup, e_stop, q_hit, shared_ns):
+    # start the remi server. Events and shared namespaces are passed via userdata. The parameter are received by main(). https://www.reddit.com/r/RemiGUI/comments/frswxg/how_to_pass_additional_arguments_through_start/
+    start(MyApp, port=8081, userdata=(e_warmup, e_stop, q_hit, shared_ns))
+
+
+def Counter(e_warmup, e_stop, q_hit, shared_ns):
+    e_warmup.wait(timeout=30.0)  # blocks and waits for the warmup finished event
+    if not e_warmup.is_set():  # check if it was the timeout
+        print("ERROR Timeout")
+        return
+
+    i = 0
+    while True:
+        i += 1
+        time.sleep(1)
+        shared_ns.ii = i
+        print("numbertwo: set shared_ns.ii", i)
+        if e_stop.is_set():
+            return
+
+        try:
+            hit = q_hit.get_nowait()
+            print("Counter: received a hit", hit.get("x_pixel"), hit.get("y_pixel"))
+        except queue.Empty:
+            pass
+
+
+def Warmup(e_warmup):
+    time.sleep(10)
+    e_warmup.set()
+
+
+if __name__ == "__main__":
+
+    with multiprocessing.Manager() as manager:
+        event_warmup = manager.Event()
+        event_stop = manager.Event()
+        queue_hit = manager.Queue(maxsize=1)
+
+        namespace = manager.Namespace()
+        namespace.name = "asdf"
+        namespace.ii = 0
+
+        myServer(e_warmup=event_warmup, e_stop=event_stop, q_hit=queue_hit, shared_ns=namespace)
